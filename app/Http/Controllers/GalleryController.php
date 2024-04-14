846d3c0cd389b8817617fd6600b549ccd61a087c
darlinton2000 <darlinton2000@gmail.com>
2024-04-14 16:39:10 -0300

    refactor: GalleryController

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -32,46 +32,24 @@ class GalleryController extends Controller
      */
     public function upload(Request $request): RedirectResponse
     {
-        $request->validate([
-            'title' => 'required|string|max:255|min:6',
-            'image' => [
-                'required',
-                'image',
-                'mimes:jpeg,png,jpg,gif',
-                'max:2048',
-                Rule::dimensions()->maxWidth(100)->maxHeight(100)
-            ]
-        ]);
+        $this->validateRequest($request);
 
-        if ($request->hasFile('image')) {
-            $title = $request->input('title');
-            $image = $request->file('image');
-            $name = $image->hashName();
+        $title = $request->input('title');
+        $image = $request->file('image');
 
-            try {
-                $return = $image->storePubliclyAs('uploads', $name, 'public');
-                $url = asset('storage/' . $return);
-            } catch (Exception $error) {
-                return redirect()->back()->withErrors([
-                    'error' => 'Erro 001 ao salvar a imagem. Tente novamente.'
-                ]);
-            }
+        try {
+            $url = $this->storeImageInDisk($image);
+            $dataBaseImage = $this->storeImageInDatabase($title, $url);
+        } catch (Exception $error) {
+            $this->deleteDataBaseImage($dataBaseImage);
+            $this->deleteImageFromDisk($url);
 
-            try {
-                Image::create([
-                    'title' => $title,
-                    'url' => $url
-                ]);
-            } catch (Exception $error) {
-                Storage::disk('public')->delete($return);
-
-                return redirect()->back()->withErrors([
-                    'error' => 'Erro 002 ao salvar a imagem. Tente novamente.'
-                ]);
-            }
-
-            return redirect()->route('index');
+            return redirect()->back()->withErrors([
+                'error' => 'Erro ao salvar a imagem. Tente novamente.'
+            ]);
         }
+
+        return redirect()->route('index');
     }
 
     /**
@@ -92,5 +70,79 @@ class GalleryController extends Controller
         }
 
         return redirect()->route('index');
+    }
+
+    /**
+     * Faz a validação da requisição
+     *
+     * @param Request $request
+     * @return void
+     */
+    private function validateRequest(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255|min:6',
+            'image' => [
+                'required',
+                'image',
+                'mimes:jpeg,png,jpg,gif',
+                'max:2048',
+                Rule::dimensions()->maxWidth(2000)->maxHeight(2000)
+            ]
+        ]);
+    }
+
+    /**
+     * Salva a imagem no disco
+     *
+     * @param $image
+     * @return string
+     */
+    private function storeImageInDisk($image)
+    {
+        $imageName = $image->storePubliclyAs('uploads', $image->hashName(), 'public');
+
+        return asset('storage/' . $imageName);
+    }
+
+    /**
+     * Salva a imagem no banco de dados
+     *
+     * @param $title
+     * @param $url
+     * @return Image
+     */
+    private function storeImageInDatabase($title, $url)
+    {
+        return Image::create([
+            'title' => $title,
+            'url' => $url
+        ]);
+    }
+
+    /**
+     * Deleta a imagem do disco
+     *
+     * @param $imageUrl
+     * @return void
+     */
+    private function deleteImageFromDisk($imageUrl)
+    {
+        $imagePath = str_replace(asset('storage/'), '', $imageUrl);
+
+        Storage::disk('public')->delete($imagePath);
+    }
+
+    /**
+     * Deleta a imagem do banco de dados
+     *
+     * @param $dataBaseImage
+     * @return void
+     */
+    private function deleteDataBaseImage($dataBaseImage)
+    {
+        if ($dataBaseImage) {
+            $dataBaseImage->delete();
+        }
     }
 }
